c2c1fb76a73a163567d2eef37301ceab4acc6e4e
Andrea Villaverde <camaratomoyo@hotmail.com>
2021-02-12 12:12:28 +0100

    happy valentines day

---
+++ index.js
@@ -0,0 +1,11 @@
+require('aframe');
+require('aframe-environment-component');
+require('aframe-ui-widgets');
+require('aframe-babia-components');
+require('aframe-text-geometry-component');
+
+//require('aframe-event-set-component');
+//require('aframe-log-component');
+//require('aframe-plot-component');
+//require('aframe-orbit-controls');
+//require('aframe-fps-counter-component');
